ea80de399b7e5407011cb35a0670694052d38144
tayfuncakirr <tayfun.cakirr9.com>
2025-08-30 17:20:43 +0300

    flex yapısı ve position lar kullanılarak text ve image dosyaları sayfaya eklenmistir.

---
+++ 30-agustos/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container">
+        <div className='logo'>
+        </div>
+        <img src="../public/bayrak.png" alt="left-img" />
+        <img src="../public/bayrak.png" alt="right-img" />
+        <div className='text'>
+        <p>30 AĞUSTOS</p>
+        <p>ZAFER BAYRAMI</p>
+        <p>KUTLU OLSUN</p>
+        <p>gazi mustafa kemal atatürk ve kahramanlarımıza sonsuz minnetle </p>
+         <div className='bottom-logo'>Web Sanatçısı | <img className='logo-img' src="../public/tayfuncakirwhitelogo.png" alt="" srcset="" /></div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
